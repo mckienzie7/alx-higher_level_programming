181019d84fef4aa4258dbc58e8b82ebde9868923
mckienzie7 <Michaelelsa12@gmail.com>
2023-05-11 14:11:04 -0700

    calculation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-# 2-args.py
+# 1-calculation.py
 # Brennan D Baraban <375@holbertonschool.com>
 
 if __name__ == "__main__":
-    """Print the number of and list of arguments."""
-    import sys
+    """Print the sum, difference, multiple and quotient of 10 and 5."""
+    from calculator_1 import add, sub, mul, div
 
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
