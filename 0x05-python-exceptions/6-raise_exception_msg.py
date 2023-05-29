5daa674f33c5554e03207b2eb29aab6e0bc0b97e
mckienzie7 <Michaelelsa12@gmail.com>
2023-05-29 14:20:08 -0700

    raise_exception

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 6-raise_exception_msg.py
+# Michael Solomon <mckienzie7>
+
+
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
