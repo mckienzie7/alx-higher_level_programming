6afb5dcc5fa0fb245d1afb4751e7c7341bbfe3c8
mckienzie7 <Michaelelsa12@gmail.com>
2023-06-12 12:29:43 -0700

    mandatory_task

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# Michael Solomon <mckienzie7>
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
