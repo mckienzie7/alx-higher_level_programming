c97ce4f32131a65cc14b28a05df5acacec93c1fa
Michael <Mckienzie7>
2023-07-18 12:01:01 -0700

    CREATing table

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
---CREATE database named 'hbtn_0c_0' and if it exist doesn't fail
-CREATE DATABASE IF NOT EXISTS 'hbtn_0c_0';
+-- Creates the database hbtn_0c_0.
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
