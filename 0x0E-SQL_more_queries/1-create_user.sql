9fda65f694f846d21aa993f79a2e03a030785a19
Michael <Mckienzie7>
2023-07-19 13:55:05 -0700

    CREATE_AND_GRANT-USER

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,9 @@
--- Write a script that creates the MySQL server user user_0d_1.
---  User_0d_1 should have all privileges on your MySQL server
--- The user_0d_1 password should be set to user_0d_1_pwd
--- If the user user_0d_1 already exists, your script should not fail
-CREATE USER IF NOT EXISTS user_0d_1
-IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL
-ON Classicmodels.*
-TO user_0d_1;
+-- Creates the user user_0d_1 with all privileges.
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
