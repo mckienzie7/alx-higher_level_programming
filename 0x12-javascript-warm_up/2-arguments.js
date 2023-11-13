77dbc21502bf73defb9275e096e6f50267b7dea0
root <michaelelsa12@gmail.com>
2023-11-13 17:03:51 +0000

    command line arg JS

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-if(process.argv.length === 2){
-	console.log('No argument');
-}else if(process.argv.length === 3)
-{
-	console.log('Argument found');
-}
-else
-{
-	console.log('Argument found');
+const myVar = process.argv.length;
+
+if (myVar === 2) {
+  console.log('No argument');
+} else if (myVar === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
