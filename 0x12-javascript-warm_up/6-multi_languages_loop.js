b82bb0462485d5ce0d5b1d1eb6d995eb7a9f3322
root <michaelelsa12@gmail.com>
2023-11-13 20:03:40 +0000

    loop JS

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const leng = myVar.length;
+let i = 0;
+while ( i < leng)
+{
+console.log(myVar[i]);
+i++;
+}
