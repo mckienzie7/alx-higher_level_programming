8594c872e59f9aeb5f9872825e76f272a139e479
root <michaelelsa12@gmail.com>
2023-11-14 20:00:20 +0000

    RECtangl JS

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (width, height) {
+    if (width > 0 && height > 0){
+      this.width = width;
+      this.height = height;
+   }
+  }
+};
