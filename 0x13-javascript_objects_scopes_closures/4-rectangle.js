643a83bc7a245eab4a140dc1408d3c2fe8d94844
root <michaelelsa12@gmail.com>
2023-11-14 20:33:34 +0000

    Double Rotate JS

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (width, height) {
+    if (width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  print () {
+    let length = '';
+    for (let i = 0; i < this.width; i++) {
+      length += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(length);
+    }
+  }
+  rotate () {
+    let j = this.width;
+    this.width = this.height;
+    this.height = j;
+  }
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
