c408f501b5c57ee0510af2e239cc0897e721dcdf
root <michaelelsa12@gmail.com>
2023-11-14 21:42:05 +0000

    occurance JS

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,9 +3,9 @@ exports.nbOccurences = function (list, searchElement) {
 const n  = list.length;
 let x = 0;
 for (let i = 0; i < n; i++){
-  if (list[i] == searchElement) {
+  if (list[i] === searchElement) {
     x+=1;
   }
 }
 return x;
-}  
+};  
